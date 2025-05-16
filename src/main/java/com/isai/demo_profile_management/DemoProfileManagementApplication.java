2023543b213ae528891a0d1420c412bc03ef3b54
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-05-16 10:00:32 -0500

    init

---
+++ src/main/java/com/isai/demo_profile_management/DemoProfileManagementApplication.java
@@ -0,0 +1,13 @@
+package com.isai.demo_profile_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProfileManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProfileManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isai/demo_profile_management/DemoProfileManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.demo_profile_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProfileManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
